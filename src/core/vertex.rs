4dd17e9ea59eea25f9d9fab26c9bca6c7449a9ec
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-08-29 12:02:29 -0300

    create vertex

---
+++ src/core/vertex.rs
@@ -0,0 +1,12 @@
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct Vertex {
+  pub id: String,
+}
+
+impl Vertex {
+  pub fn new(id: &str) -> Self {
+    Vertex {
+      id: id.to_string(),
+    }
+  }
+}
